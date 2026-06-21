30494d663e883f16898b1b9665c962700437f091
Kingsley Leon <kingsleyleoncs@gmail.com>
2026-06-21 15:28:17 -0400

    feat: Configured FFI for listening to audio for flutter application to use

--- backend-core/core-logic/src/audio.rs
+++ backend-core/core-logic/src/audio.rs
@@ -1,6 +1,8 @@
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::Stream;
+use std::sync::mpsc::Sender;
 
-fn audio_input() -> Result<(), Box<dyn std::error::Error>> {
+fn create_stream(tx : Sender<Vec<f32>>) -> Result<Stream, Box<dyn std::error::Error>> {
     let host = cpal::default_host();
     let device = host
         .default_input_device()
@@ -9,7 +11,19 @@ fn audio_input() -> Result<(), Box<dyn std::error::Error>> {
     let err_fn = |err| eprintln!("An error occurred on the audio stream: {}", err);
     let sample_format = config.sample_format();
     let config: cpal::StreamConfig = config.into();
-    Ok(())
+    let stream = match sample_format {
+        cpal::SampleFormat::F32 => device.build_input_stream(
+            &config,
+            move |data: &[f32], _: &cpal::InputCallbackInfo| {
+                let _ = tx.send(data.to_vec()); 
+            },
+            err_fn,
+            None,
+        )?,
+        _ => panic!("Unsupported sample format! (Expected f32)"),
+    };
+    stream.play()?; 
+    Ok(stream)
 }
 
 
--- backend-core/native-ffi/src/lib.rs
+++ backend-core/native-ffi/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+pub extern "C" fn listen_audio() {
+    let (tx, rx) = std::sync::mpsc::channel::<Vec<f32>>();
+    std::thread::spawn(move || {
+        core_logic::start_processing_loop(rx);
+    });
+    let _my_live_stream = create_stream(tx).expect("Failed to initialize audio input!");
+    std::thread::sleep(std::time::Duration::from_secs(10));
 }
